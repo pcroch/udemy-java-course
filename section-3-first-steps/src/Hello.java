d2e4b229af1e3458626c6c4b324188cc43c21735
pierre crochet <pierrecrochet03@gmail.com>
2021-01-12 11:46:53 +0000

    variables

--- section-3-first-steps/src/Hello.java
+++ section-3-first-steps/src/Hello.java
@@ -1,7 +1,10 @@
 public class Hello {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello , Pierre");
+
+        int myFirstNumber = (5 + 10);
+        System.out.println(myFirstNumber);
     }
 }
 
